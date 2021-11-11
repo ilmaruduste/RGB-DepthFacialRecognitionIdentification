458f815f91cf01a56477b0012f2cae0e52dc49e5
Ilmar Uduste <ilmaruduste@gmail.com>
2021-11-11 16:48:17 +0100

    Add frontal facial recognition to simple camera feed

--- camera_feed.py
+++ camera_feed.py
@@ -13,6 +13,21 @@ print("Camera feed is up! Try pressing any button in the feed to take a new pict
 while rval:
     cv2.imshow("preview", frame)
     rval, frame = vc.read()
+
+    classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # perform face detection
+    bboxes = classifier.detectMultiScale(frame)
+    # print bounding box for each detected face
+    for box in bboxes:
+        # extract
+        x, y, width, height = box
+        x2, y2 = x + width, y + height
+        # draw a rectangle over the pixels
+        cv2.rectangle(frame, (x, y), (x2, y2), (0,0,255), 1)
+    # show the image
+    cv2.imshow('face detection', frame)
+
+
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
