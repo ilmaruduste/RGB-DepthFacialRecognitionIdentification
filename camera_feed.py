5c1b0c3c97f9569fc2b180133ee2d9f1f135c284
Ilmar Uduste <ilmaruduste@gmail.com>
2021-11-11 16:40:05 +0100

    Implement simple camera feed for testing

---
+++ camera_feed.py
@@ -0,0 +1,21 @@
+import cv2
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+print("Camera feed is up! Try pressing any button in the feed to take a new picture.")
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+
+vc.release()
+cv2.destroyWindow("preview")
